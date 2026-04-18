f6f631b460a6f9c35f765243f21b862fee0c9dff
David Stolarsky <david.stolarsky@gmail.com>
2026-04-18 14:31:53 -0400

    Add head metadata for shuffle formula

--- Formula/shuffle.rb
+++ Formula/shuffle.rb
@@ -4,6 +4,7 @@ class Shuffle < Formula
   url "https://github.com/ddnn55/shuffle/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "52e9f75bf4ed4935b14130e5f69971dafd00dc28bc4867fe18d3879c12b1b76e"
   license "MIT"
+  head "https://github.com/ddnn55/shuffle.git", branch: "main"
 
   depends_on "rust" => :build
   depends_on :macos
